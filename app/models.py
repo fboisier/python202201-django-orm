b07e4c1fa032f7f6c503c55a312cf0f8574d0446
fboisier <francisco.boisier.sandoval@gmail.com>
2022-02-09 21:57:14 -0300

    primer commit

---
+++ app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Ciudad(models.Model):
+    nombre = models.CharField(max_length=50)
+    cantidad_habitantes = models.IntegerField(default=10, unique=True)
+    temperatura = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.nombre} C.H.:{self.cantidad_habitantes} MM"
+
+class Region(models.Model):
+    nombre = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class Movie(models.Model):
+    title = models.CharField(max_length=45)
+    description = models.TextField()
+    release_date = models.DateTimeField()
+    duration = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
